181d4f79fe748d57b9bad9473f1eb2789a314df3
Nijat Hamidov <hamidov9jat@gmail.com>
2022-09-06 17:15:08 +0400

    some reformatting

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-
 question_bank = []
 for q_and_a in question_data:
     question_bank.append(Question(q_and_a['text'], q_and_a['answer'].lower()))
