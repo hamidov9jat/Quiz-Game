efa4820a01b7a45d83e8d3f1f39299aab82ac318
Nijat Hamidov <hamidov9jat@gmail.com>
2022-09-06 17:23:42 +0400

    added more data

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for q_and_a in question_data:
-    question_bank.append(Question(q_and_a['text'], q_and_a['answer'].lower()))
+    question_bank.append(Question(q_and_a['question'], q_and_a['correct_answer'].lower()))
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
-    is_exit = input("Do you want to exit 'y' or 'smth else'? ").lower()
+    is_exit = input("Do you want to exit 'y' or 'smth else to continue'? ").lower()
     if is_exit == 'y':
         break
 print('Goodbye!')
